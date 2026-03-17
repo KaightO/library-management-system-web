529f4cbff01a8b78ed038db15b90a6216754c337
“B15hesh” <bisheshs377@gmail.com>
2026-03-17 18:39:40 +0545

    password change

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 
 $host = getenv('DB_HOST') ?: '127.0.0.1';
 $user = getenv('DB_USER') ?: 'root';
-$pass = getenv('DB_PASS') ?: 'Root@123';
+$pass = getenv('DB_PASS') ?: '';
 $db   = getenv('DB_NAME') ?: 'library_db';
 
 $conn = new mysqli($host, $user, $pass, $db);
