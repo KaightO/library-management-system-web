2b1040d9e0f2e5a03bfec195ec40eecc0c3d76fc
“Ritsav” <60186486+Ritsav@users.noreply.github.com>
2026-03-19 07:55:13 +0545

    fix db_connect as env isn't necessary

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = getenv('DB_HOST') ?: '127.0.0.1';
-$user = getenv('DB_USER') ?: 'root';
-$pass = getenv('DB_PASS') ?: '';
-$db   = getenv('DB_NAME') ?: 'library_db';
+$host = '127.0.0.1';
+$user = 'root';
+$pass = '';
+$db   = 'library_db';
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
